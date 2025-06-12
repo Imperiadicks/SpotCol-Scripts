9cea7bcef9f2093ea685ba2301b1a75fad05c096
Александр <prokofevlopotras321@gmail.com>
2025-06-12 17:31:30 +0300

    фыв

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
 ;(function initSpotCol() {
   // Базовый URL репозитория на jsDelivr
-  const BASE = 'https://cdn.jsdelivr.net/gh/Imperiadicks/SpotCol-Scripts@6cbf95f';
+  const BASE = 'https://cdn.jsdelivr.net/gh/Imperiadicks/SpotCol-Scripts@0b178de';
   const THEME_ID      = 'SpotColЛичная';
   const POLL_INTERVAL = 1000;
 
