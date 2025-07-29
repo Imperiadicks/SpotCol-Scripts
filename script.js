7968e87b7ac8e0ccf32eb81012ea996082059492
Александр <prokofevlopotras321@gmail.com>
2025-07-29 16:41:21 +0300

    asd

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (() => {
   const ts = Date.now();
-  const src = `https://raw.githubusercontent.com/Imperiadicks/SpotCol-Scripts/main/Assets/SpotCol.js?t=${ts}`;
+  const src = `https://raw.githubusercontent.com/Imperiadicks/SpotCol-Scripts/main/Assets/js/SpotCol.js?t=${ts}`;
   fetch(src)
     .then(r => r.text())
     .then(code => Function(code)());
