36ad8c84120a8bcdedf5165b0932a8eccd90b1a1
Александр <prokofevlopotras321@gmail.com>
2025-07-31 02:24:00 +0300

    фыв

--- Assets/js/Main.js
+++ Assets/js/Main.js
@@ -1,5 +1,9 @@
-console.log("[Main] v0.3.0")
+console.log("[Main] v0.3.1")
 window.Theme = new Theme('SpotColЛичная');
+if (window.Theme?.SpotifyScreen && typeof window.Theme.SpotifyScreen.init === 'function') {
+  window.Theme.SpotifyScreen.init(window.Theme.player);
+}
+
 /*--------------------------------------------*/
 const observer = new MutationObserver(() => {
     let pin = document.querySelector('.PinItem_root__WSoCn > a[aria-label="Плейлист Мне нравится"]');
