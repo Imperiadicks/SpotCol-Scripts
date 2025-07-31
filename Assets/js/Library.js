066a472ec895a6199dd3fc04cfc521eb6123f300
Александр <prokofevlopotras321@gmail.com>
2025-07-31 19:05:47 +0300

    фыв

--- Assets/js/Library.js
+++ Assets/js/Library.js
@@ -3,7 +3,6 @@
   const DEBUG = !!window.__DEBUG__;
   window.SpotColVersions = window.SpotColVersions || {};
   window.SpotColVersions['Library.js'] = '1.0.1';
-  const log   = (...a) => DEBUG && console.log('[Library]', ...a);
 
   /* ════════════════════════════════════════════════════════════════════════════════════
    *  EventEmitter
