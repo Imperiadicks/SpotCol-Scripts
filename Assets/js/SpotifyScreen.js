338370bd0f05921baec6354f392d6d33ec16f1b9
Александр <prokofevlopotras321@gmail.com>
2025-08-01 13:51:19 +0300

    asd

--- Assets/js/SpotifyScreen.js
+++ Assets/js/SpotifyScreen.js
@@ -1,5 +1,5 @@
 const SpotColЛичная = window.Theme;
-console.log("проверка SPOTIFYSCREEN 0.2.7")
+console.log("проверка SPOTIFYSCREEN 0.2.8")
 if (!SpotColЛичная) {
   console.error("[SpotifyScreen] Theme is not available.");
   throw new Error("Theme not loaded");
@@ -102,7 +102,7 @@ const build = () => {
   $root = null;
   $bg = $cover = $track = $like = $artist = null;
 
-  const layout = document.querySelector('[class*="CommonLayout_content__zy_Ja"]');
+  const layout = document.querySelector('[class*="Content_main__8_wIa"]');
   const sibling = layout?.nextElementSibling;
 
   $root = el('div', 'Spotify_Screen');
