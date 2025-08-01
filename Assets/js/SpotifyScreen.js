b251d487306d664db2c6bd67eea68cfcbc9fbbb9
Александр <prokofevlopotras321@gmail.com>
2025-08-01 03:50:16 +0300

    фыв

--- Assets/js/SpotifyScreen.js
+++ Assets/js/SpotifyScreen.js
@@ -1,5 +1,5 @@
 const SpotColЛичная = window.Theme;
-console.log("проверка SPOTIFYSCREEN 0.2.5")
+console.log("проверка SPOTIFYSCREEN 0.2.6")
 if (!SpotColЛичная) {
   console.error("[SpotifyScreen] Theme is not available.");
   throw new Error("Theme not loaded");
