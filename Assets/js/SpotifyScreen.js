c57ff11a40caf9569b0ec982b3139a9d69074fb5
Александр <77098834+Imperiadicks@users.noreply.github.com>
2025-08-05 22:20:58 +0300

    Update SpotifyScreen.js
    Обновление версии с 0.2.6 --> 0.3.0

--- Assets/js/SpotifyScreen.js
+++ Assets/js/SpotifyScreen.js
@@ -1,5 +1,5 @@
 const SpotColЛичная = window.Theme;
-console.log("проверка SPOTIFYSCREEN 0.2.6")
+console.log("проверка SPOTIFYSCREEN 0.3.0")
 if (!SpotColЛичная) {
   console.error("[SpotifyScreen] Theme is not available.");
   throw new Error("Theme not loaded");
